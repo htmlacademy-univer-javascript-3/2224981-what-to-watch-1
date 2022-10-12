896e867476a83ffb20e9ae7ca277ff556a874952
SeriousDim <dlykov2001@yandex.ru>
2022-10-12 20:09:27 +0500

    Created VideoPlayer component, task 5.17 done

--- project/src/components/film-card/film-card.tsx
+++ project/src/components/film-card/film-card.tsx
@@ -1,6 +1,8 @@
 import FilmInfo from '../../types/film-info';
 import {Link} from 'react-router-dom';
 import AppRoutes from '../../const/app-routes';
+import VideoPlayer from '../video-player/video-player';
+import {useState} from 'react';
 
 type FilmCardProps = {
   filmInfo: FilmInfo;
@@ -9,10 +11,32 @@ type FilmCardProps = {
 }
 
 function FilmCard(props: FilmCardProps): JSX.Element {
+  const [showPreview, setShowPreview] = useState(false);
+
+  function onMouseEnterWrapper() {
+    if (props.onMouseEnterHandler) {
+      props.onMouseEnterHandler();
+    }
+
+    setShowPreview(true);
+  }
+
+  function onMouseLeaveWrapper() {
+    if (props.onMouseLeaveHandler) {
+      props.onMouseLeaveHandler();
+    }
+
+    setShowPreview(false);
+  }
+
   return (
-    <article className="small-film-card catalog__films-card" onMouseEnter={props.onMouseEnterHandler} onMouseLeave={props.onMouseLeaveHandler}>
+    <article className="small-film-card catalog__films-card" onMouseEnter={onMouseEnterWrapper} onMouseLeave={onMouseLeaveWrapper}>
       <div className="small-film-card__image">
-        <img src={props.filmInfo.previewImage} width="280" height="175" />
+        {
+          showPreview
+            ? <VideoPlayer previewImage={props.filmInfo.previewImage} videoLink={props.filmInfo.videoLink} playSound={false} width={280} height={175} delayMs={1000}/>
+            : <img src={props.filmInfo.previewImage} width="280" height="175" />
+        }
       </div>
       <h3 className="small-film-card__title">
         <Link className="small-film-card__link" to={AppRoutes.FilmsRoot + props.filmInfo.id}>{props.filmInfo.name}</Link>
